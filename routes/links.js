81b622582d063bcd8bda52f40fbbfb9a3b895a15
anstcher <anastasiacher73@gmail.com>
2023-02-15 12:18:53 +0100

    CRUD acabado

--- routes/links.js
+++ routes/links.js
@@ -6,16 +6,56 @@ const pool = require('../database')
 
 /* GET users listing. */
 router.get('/', async (req, res, next) => {
-  const [ result ] = await pool.query('SELECT 1+1')
-  res.json(result)
+  const [ links ] = await pool.query('SELECT * FROM links')
+  console.log(links)
+  res.render('links/list', { links })
+  
 });
 
 router.get('/add', (req, res) => {
   res.render('links/add')
 })
 
-router.post('/add', (req, res) => {
-  res.send('recibido')
+router.post('/add', async (req, res) => {
+  const { title, url, descripcion } = req.body
+  const newLink = {
+    title,
+    url,
+    descripcion
+  }
+
+  // console.log(newLink)
+  await pool.query('INSERT INTO links SET ?', [newLink])
+  res.redirect('/links')
+})
+
+router.get('/delete/:id', async (req, res) => {
+  // console.log(req.params.id)
+  const { id } = req.params
+  await pool.query('DELETE FROM links WHERE id = ?', [ id ])
+  res.redirect('/links')
+})
+
+router.get('/edit/:id', async (req, res) => {
+  // console.log(req.params.id)
+  const { id } = req.params
+  const [ link ] = await pool.query('SELECT * FROM links WHERE id = ?', [ id ])
+  console.log(link)
+  // pasamos el  link al edir.hbs
+  res.render('links/edit', { link:link[0]})
+})
+
+router.post('/edit/:id', async (req, res) => {
+  const { id } = req.params
+  const { title, url, descripcion } = req.body
+  const newLink = {
+    title,
+    url,
+    descripcion
+  }
+  // console.log(newLink)
+  await pool.query('UPDATE links SET ? WHERE id = ?',[newLink, id])
+  res.redirect('/links')
 })
 
 
